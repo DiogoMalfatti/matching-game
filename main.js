492d829e5aa190dd7aef28637798d3ad5a1541df
Diogo Malfatti <diimalfatti@gmail.com>
2021-08-03 19:46:41 -0300

    💄 Create ScoreBoard object without CSS

--- main.js
+++ main.js
@@ -2,15 +2,14 @@ import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
 import './src/styles/settings/colors.css'
 import BoardGame from './src/objects/BoardGame'
-import PlayerName from './src/components/PlayerName'
+import ScoreBoard from './src/objects/ScoreBoard'
 
 const $root = document.querySelector('#root')
 
 $root.insertAdjacentHTML(
   'beforeend',
   `
-  ${PlayerName('Player 1')}
-  ${PlayerName('Player 2')}
+  ${ScoreBoard()}
   ${BoardGame(6)}
   `,
 )
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,6 +1,11 @@
+import './styles.css'
+import PlayerName from '../../components/PlayerName'
+
 function ScoreBoard() {
   return /*html*/ `
   <header class='score-board'>
+    ${PlayerName('Player 1')}
+    ${PlayerName('Player 2')}
   </header>
   `
 }
